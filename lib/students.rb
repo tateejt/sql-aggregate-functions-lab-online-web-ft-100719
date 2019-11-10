5d83a7d21c5bc21d0b1104d2748ebd8239cb0b9c
Emery Tate <tateejt@gmail.com>
2019-11-10 03:38:00 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "Select Avg(gpa) From students"
 end
 
 def total_tardies_for_all_students
+  "Select Sum(gpa) From students"
 end
 
 def average_gpa_for_9th_grade
+  "Select Avg"
 end
